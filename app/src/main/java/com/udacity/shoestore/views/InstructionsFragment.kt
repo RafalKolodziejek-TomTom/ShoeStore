81377ee19ccfa571886787a5ee18555ebff6e5b1
RafalKolodziejek-TomTom <rafal.kolodziejek@tomtom.com>
2022-08-23 21:27:37 +0200

    add project files

---
+++ app/src/main/java/com/udacity/shoestore/views/InstructionsFragment.kt
@@ -0,0 +1,32 @@
+package com.udacity.shoestore.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
+
+class InstructionsFragment : Fragment() {
+
+    private lateinit var binding: FragmentInstructionsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_instructions, container, false
+        )
+        binding.shoeListButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_instructionsFragment_to_shoeListFragment)
+        )
+        return binding.root
+    }
+}
